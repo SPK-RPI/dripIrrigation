4d255d0107f34be744a92cd689bf5f4d84ff8289
SPK-RPI <shibaprasadkar50@gmail.com>
2018-12-03 19:06:18 +0530

    first commit

---
+++ irrigation_Soil_moisture.ino
@@ -0,0 +1,39 @@
+
+int sensor_pin = A0;
+
+int output_value;
+int led = 7;
+
+void setup()
+{
+   Serial.begin(9600);
+
+   Serial.println("Reading From the Sensor ...");
+   pinMode(led, OUTPUT);
+       delay(2000);
+}
+
+void loop()
+{
+   output_value = analogRead(sensor_pin);
+
+   output_value = map(output_value, 550, 0, 0, 100);
+
+   Serial.print("Mositure : ");
+   if (output_value == -86)//hypothatical value....
+   {
+      digitalWrite(led, HIGH);
+      Serial.println("if condition");//dbuding purpose
+   }
+   else
+   {
+      Serial.println("else condition");
+   }
+   digitalWrite(led, LOW);
+
+   Serial.print(output_value);
+
+   Serial.println("%");
+
+   delay(1000);
+}
